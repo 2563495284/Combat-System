9dd96a0d9ec8a41e41e7b8221cf244aa72db2a44
yijin <359222976@qq.com>
2025-12-30 02:24:48 +0800

    *

--- Assets/Script/FacingCamera.cs
+++ Assets/Script/FacingCamera.cs
@@ -9,13 +9,40 @@ using UnityEngine;
 public class FacingCamera : MonoBehaviour
 {
     Transform[] childs;
+    SpriteRenderer[] spriteRenderers;
+    BoxCollider2D[] boxColliders; // BoxCollider2D 组件数组
+    bool[] isDynamic; // 标记每个子对象是否为动态物体
+    
+    [Header("Order in Layer 设置")]
+    [Tooltip("基础 Order in Layer 值")]
+    public int baseOrderInLayer = 0;
+    
+    [Tooltip("Y 坐标每单位对应的 Order in Layer 增量（通常为负值，Y越大order越大）")]
+    public float yToOrderMultiplier = -100f;
     
     void Start()
     {
-        childs = new Transform[transform.childCount];
-        for (int i = 0; i < transform.childCount; i++)
+        int childCount = transform.childCount;
+        childs = new Transform[childCount];
+        spriteRenderers = new SpriteRenderer[childCount];
+        boxColliders = new BoxCollider2D[childCount];
+        isDynamic = new bool[childCount];
+        
+        for (int i = 0; i < childCount; i++)
         {
             childs[i] = transform.GetChild(i);
+            spriteRenderers[i] = childs[i].GetComponent<SpriteRenderer>();
+            boxColliders[i] = childs[i].GetComponent<BoxCollider2D>();
+            
+            // 通过检查 Rigidbody2D 或 Rigidbody 组件来判断是否为动态物体
+            isDynamic[i] = childs[i].GetComponent<Rigidbody2D>() != null;
+            
+            // 静态物体：初始化 order in layer
+            if (!isDynamic[i] && spriteRenderers[i] != null)
+            {
+                int orderInLayer = CalculateOrderInLayer(i);
+                spriteRenderers[i].sortingOrder = orderInLayer;
+            }
         }
     }
 
@@ -29,8 +56,39 @@ public class FacingCamera : MonoBehaviour
         {
             if (childs[i] != null)
             {
+                // 更新旋转
                 childs[i].rotation = Camera.main.transform.rotation;
+                
+                // 动态物体：动态更新 order in layer
+                if (isDynamic[i] && spriteRenderers[i] != null)
+                {
+                    int orderInLayer = CalculateOrderInLayer(i);
+                    spriteRenderers[i].sortingOrder = orderInLayer;
+                }
             }
         }
+    }
+    
+    /// <summary>
+    /// 根据 BoxCollider2D 的底部 Y 坐标计算 Order in Layer
+    /// </summary>
+    /// <param name="index">子对象索引</param>
+    /// <returns>Order in Layer 值</returns>
+    private int CalculateOrderInLayer(int index)
+    {
+        float bottomY;
+        
+        // 优先使用 BoxCollider2D 的底部
+        if (boxColliders[index] != null)
+        {
+            bottomY = boxColliders[index].bounds.min.y;
+        }
+        else
+        {
+            // 如果没有 BoxCollider2D，回退到使用 transform.position.y
+            bottomY = childs[index].position.y;
+        }
+        
+        return baseOrderInLayer + Mathf.RoundToInt(bottomY * yToOrderMultiplier);
     }
 }
